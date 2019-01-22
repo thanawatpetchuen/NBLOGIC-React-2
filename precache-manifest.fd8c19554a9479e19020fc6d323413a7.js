2ad9c3532b208ded2016561e68d2698082037b52
Thanawat Petchuen <thanawat1petchuen@gmail.com>
2019-01-23 00:02:38 +0700

    First Commit

---
+++ precache-manifest.fd8c19554a9479e19020fc6d323413a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/NBLOGIC-React/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5350014d30f13f5236f2",
+    "url": "/NBLOGIC-React/static/js/runtime~main.5350014d.js"
+  },
+  {
+    "revision": "5f05246e82a7cf6c9b83",
+    "url": "/NBLOGIC-React/static/js/main.5f05246e.chunk.js"
+  },
+  {
+    "revision": "09cf520d47b7d122dd91",
+    "url": "/NBLOGIC-React/static/js/1.09cf520d.chunk.js"
+  },
+  {
+    "revision": "5f05246e82a7cf6c9b83",
+    "url": "/NBLOGIC-React/static/css/main.ec683c01.chunk.css"
+  },
+  {
+    "revision": "737ae267fde6d5b931dbd52314bc4f47",
+    "url": "/NBLOGIC-React/index.html"
+  }
+];
